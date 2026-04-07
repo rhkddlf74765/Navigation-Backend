52054b13b7e9c7b2f372e65e241085b16cc4af3e
Park GwangIl <rhkddlf74765@naver.com>
2026-04-07 13:41:50 +0900

    Refactor: Move old backend to experimental and initialize new backend

---
+++ src/main/java/com/example/campus_navigation_backend/CampusNavigationBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.campus_navigation_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusNavigationBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusNavigationBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/campus_navigation_backend/CampusNavigationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.campus_navigation_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusNavigationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
