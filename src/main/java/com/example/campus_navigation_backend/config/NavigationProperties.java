2055b3b07cc3559493105539041cebea4ebe5a9a
Park GwangIl <rhkddlf74765@naver.com>
2026-07-01 02:57:25 +0900

    test ci

--- src/main/java/com/example/campus_navigation_backend/config/NavigationProperties.java
+++ src/main/java/com/example/campus_navigation_backend/config/NavigationProperties.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import java.util.List;
 
 /**
- * 시스템에서 전역적으로 사용할 수 있는 설정 파일
+ * 시스템에서 전역적으로 사용하는 설정 파일
  * @param requestSrid
  * @param metricSrid
  * @param candidateRadiusMeters
