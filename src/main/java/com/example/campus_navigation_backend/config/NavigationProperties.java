63c119a818659d082a0356d3c968ee487240b56b
Park GwangIl <rhkddlf74765@naver.com>
2026-07-01 02:54:22 +0900

    chore: force trigger CI with test file

--- src/main/java/com/example/campus_navigation_backend/config/NavigationProperties.java
+++ src/main/java/com/example/campus_navigation_backend/config/NavigationProperties.java
@@ -4,6 +4,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 
+/**
+ * 시스템에서 전역적으로 사용할 수 있는 설정 파일
+ * @param requestSrid
+ * @param metricSrid
+ * @param candidateRadiusMeters
+ * @param maxCandidates
+ * @param walkableHighways
+ */
 @ConfigurationProperties(prefix = "navigation")
 public record NavigationProperties(
     int requestSrid,
